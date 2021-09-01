ab5936995613929e53f52a78f8a1e51fcb87c5a2
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-08-31 20:42:20 -0400

    5.1 - handle login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,13 +16,81 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception);
+    }
+  }
+
+  const handleBlogChange = (event) => {
+    setNewBlog(event.target.value)
+  }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type='text'
+          value={username}
+          name='Username'
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type='password'
+          value={password}
+          name='Password'
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  )
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <input
+        value={newBlog}
+        onChange={handleBlogChange}
+      />
+      <button type='submit'>save</button>
+    </form>
+  )
+
+  if (user === null) {    // conditional app if user isn't logged in
+    return (
+      <div>
+        <h2>log in to application</h2>
+        {loginForm()}
+    </div>
+    )
+  }
+
   return (
     <div>
+      {blogForm()}
       <h2>blogs</h2>
+      <p>{user.name} logged-in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
-    </div>
+  </div>
   )
 }
 
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from '../utils/axios_config'
 const baseUrl = '/api/blogs'
 
 const getAll = () => {
